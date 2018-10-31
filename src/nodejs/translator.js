11be393d8344f294c4c1bc7d77e706b111383127
petrov <s.petrov@f5.com>
2018-10-30 23:41:54 -0700

    Basic translator functions to build mapping and translate data

--- src/nodejs/translator.js
+++ src/nodejs/translator.js
@@ -8,34 +8,78 @@
 
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const logger = require('./logger.js'); // eslint-disable-line no-unused-vars
+
+const consumersDir = './consumers';
 
 
 /**
- * Build set of data translation mappings for each consumer
- * @param {array} consumers
+ * Build data translation mappings for each consumer.
+ * Should be called on start or when configuration changed only.
  *
- * @returns {array} translation mapping for each consumer
+ * @param {Object}   config                      - object with configuration data
+ * @param {Object[]} config.consumers            - list of consumers
+ * @param {string}   config.consumers[].consumer - consumer's type/name
+ *
+ * @returns {Object} Promise which is resolved with the translation
+ *                   mapping for each consumer
  */
-function buildTranslationMaps(consumers) {
-    // build mapping for each consumer
-}
+async function buildTranslationMaps(config) {
+    logger.info('Building translation mapping for new config');
 
+    return Promise.all(config.consumers.map(async (consumerObj) => {
+        const consumer = consumerObj.consumer;
+        const consumerDir = path.join(consumersDir, consumer, 'mapping');
+        if (fs.existsSync(consumerDir)) {
+            logger.debug(`Consumer "${consumer}", mapping dir - ${consumerDir}`);
+
+            return new Promise((resolve) => {
+                fs.readdir(consumerDir, (err, files) => {
+                    const mapping = {};
+
+                    files.forEach((fname) => {
+                        if (path.extname(fname) === '.js') {
+                            const sourceType = path.basename(fname, '.js');
+                            const modulePath = './'.concat(path.join(consumerDir, fname));
+
+                            logger.debug(`Consumer "${consumer}", attempt to load module ${modulePath}`);
+                            mapping[sourceType] = require(modulePath).translator;
+
+                            logger.debug(`Consumer "${consumer}", supports sourceType - ${sourceType}`);
+                        }
+                    });
+                    resolve({ [consumer]: mapping });
+                });
+            });
+        }
+        logger.error(`Consumer "${consumer}" has no valid mapping dir at "${consumerDir}"`);
+        return Promise.resolve({ [consumer]: {} });
+    }));
+}
 
 /**
  * Translate data to consumer's format
- * @param {string} consumer   - consumer name
- * @param {Object} data       - data to transform
+ * @param {Object} mapping    - translation mapping for consumer
  * @param {string} sourceType - data's sourceType
+ * @param {Object} data       - data to transform
  *
- * @returns {Object} data translated to consumer's format
+ * @returns {Object} Promise which is resolved with the translated data
  */
-function translate(consumer, data, sourceType) {
-    // translate data from normazlied to consumer's format
+function translateData(mapping, sourceType, data) {
+    return new Promise((resolve, reject) => {
+        if (mapping[sourceType] === undefined) {
+            const error = `Missing sourceType: mapping has no sourceType "${sourceType}"`;
+            logger.error(`translateData error: ${error}`);
+            reject(new Error(error));
+        }
+        resolve(mapping[sourceType](data));
+    });
 }
 
 
 module.exports = {
-    translate: translate,
-    buildTranslationMaps: buildTranslationMaps
+    buildMapping: buildTranslationMaps,
+    translate: translateData
 };
