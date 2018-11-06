5eb9ecaf5c73856eb7764e89815c59b3dfd3b663
petrov <s.petrov@f5.com>
2018-11-06 00:47:49 -0800

    Added constants file for Splunk specific constants

---
+++ src/nodejs/consumers/Splunk/constants.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2018. F5 Networks, Inc. See End User License Agreement ("EULA") for
+ * license terms. Notwithstanding anything to the contrary in the EULA, Licensee
+ * may copy and modify this software product for its internal business purposes.
+ * Further, Licensee may upload, publish and distribute the modified version of
+ * the software product on devcentral.f5.com.
+ */
+
+'use strict';
+
+module.exports = {
+    maxDataChunkSize: 99000,
+    sourceTypes: {
+        dir: './sourceTypes',
+        ext: '.js'
+    },
+    api: {
+        postData: '/services/collector/event'
+    }
+};
