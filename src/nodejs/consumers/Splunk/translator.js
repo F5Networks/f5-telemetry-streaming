07b19cb60cda3ca66f859b0623e11c73e03b1e3f
petrov <s.petrov@f5.com>
2018-11-06 01:00:38 -0800

    Added support for constants; overall summary sourceType

--- src/nodejs/consumers/Splunk/translator.js
+++ src/nodejs/consumers/Splunk/translator.js
@@ -10,13 +10,15 @@
 
 const fs = require('fs');
 const path = require('path');
+
+const constants = require('./constants.js');
 const logger = require('./logger.js'); // eslint-disable-line no-unused-vars
 
 // Load all sourceTypes at initialization just once
-const sourceTypesDir = './sourceTypes';
-const moduleExt = '.js';
-
 const sourceTypes = (function loadSourceTypes() {
+    const sourceTypesDir = constants.sourceTypes.dir;
+    const sourceTypeExt = constants.sourceTypes.ext;
+
     // absolute path to sourceTypes dir is required
     const absolutePath = path.join(path.dirname(__filename), sourceTypesDir);
     const loadedSourceTypes = {};
@@ -26,10 +28,10 @@ const sourceTypes = (function loadSourceTypes() {
         logger.debug(`Loading module ${modulePath}`);
 
         try {
-            loadedSourceTypes[path.basename(fname, moduleExt)] = require(modulePath);
+            loadedSourceTypes[path.basename(fname, sourceTypeExt)] = require(modulePath);
         } catch (err) {
             logger.error(`Unable to load ${modulePath}. Detailed error:\n`, err);
-            return
+            return;
         }
         logger.debug(`Module ${modulePath} - loaded!`);
     });
@@ -37,38 +39,70 @@ const sourceTypes = (function loadSourceTypes() {
 }());
 
 
+async function addReportStats(request, translatedData) {
+    const data = request.data;
+    // bytes_transfered = data length + ',' * translatedData.length - 1 (no ',' after last element)
+    return {
+        time: data.timestamp,
+        host: data.hostname,
+        index: data.rbac_system_index,
+        source: 'bigip.tmsh.stats.summary',
+        sourcetype: 'f5:bigip:stats:iapp:json',
+        event: {
+            aggr_period: data.aggregationPeriod,
+            devicegroup: data.deviceGroup,
+            facility: data.facility,
+            files_sent: request.context.numberOfRequests,
+            bytes_transfered: request.context.dataLength + translateData.length - 1
+        }
+    };
+}
+
+
 async function translateData(data, consumer) {
     logger.debug('Incoming data for translation');
     const translatedData = [];
-    const maxPromises = 2;
-    let promises = [];
-
-    Object.keys(sourceTypes).forEach((sourceType) => {
-        if (promises.length === maxPromises) {
-            Promise.all(promises).catch((err) => {
-                logger.error(`translateData error: ${err}\nDetailed error info:\n`, err);
-            });
-            promises = [];
+    const request = {
+        data,
+        context: {
+            consumer,
+            dataLength: 0,
+            currentChunkLength: 0,
+            numberOfRequests: 0
         }
-        promises.push(new Promise((resolve, reject) => {
-            sourceTypes[sourceType](data, consumer).then((res) => {
-                if (res !== undefined) {
-                    if (Array.isArray(res)) {
-                        res.forEach(part => translatedData.push(part));
-                    } else {
-                        translatedData.push(res);
-                    }
+    };
+    const appendData = function (newData) {
+        const context = request.context;
+        const newDataStr = JSON.stringify(newData);
+        
+        context.currentChunkLength += newDataStr.length;
+        if (context.currentChunkLength >= constants.maxDataChunkSize) {
+            context.dataLength += context.currentChunkLength;
+            context.currentChunkLength = 0;
+            context.numberOfRequests++;
+        }
+        translatedData.push(newDataStr);
+    };
+
+    const promises = Object.keys(sourceTypes)
+        .map(sourceType => new Promise((resolve) => {
+            resolve(sourceTypes[sourceType](request, consumer));
+        }).then((res) => {
+            if (res !== undefined) {
+                if (Array.isArray(res)) {
+                    res.forEach(part => appendData(part));
+                } else {
+                    appendData(res);
                 }
-                resolve();
-            }).catch(err => console.log(`translateData::${sourceType} error: ${err}.\nDetailed error info:\n`, err));
-        }));
-    });
-    if (promises.length) {
-        Promise.all(promises).catch((err) => {
-            logger.error(`translateData error: ${err}\nDetailed error info:\n`, err);
+            }
+        }).catch(err => logger.error(`translateData::${sourceType} error: ${err}\nDetailed error info:\n`, err)));
+
+    return Promise.all(promises)
+        .then(() => addReportStats(request, translatedData))
+        .then((res) => {
+            appendData(res);
+            return translatedData;
         });
-    }
-    return translatedData;
 }
 
 
